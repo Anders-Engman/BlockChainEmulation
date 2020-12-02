79b0976f78953fac424200b8f873567f4a5c7704
A-Engman <anders.p.engman@gmail.com>
2020-12-02 16:26:53 -0500

    Removed Redundant Files
    Removed some redundant files including a duplicate pom.xml and basic MVC files

---
+++ blockchainemulator/src/main/java/com/model/HashGenerator.java
@@ -0,0 +1,10 @@
+package com.java.model;
+
+public class HashGenerator {
+    
+    public byte[] generateHashFromString() {
+        byte[] hash = new byte[64];
+        // implementation
+        return hash;
+    }
+}
