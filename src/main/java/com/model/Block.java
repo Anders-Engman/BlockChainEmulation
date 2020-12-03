4394865ef1c5f02191d4ed313d696f9699de1cd4
rmathewes <75384595+rmathewes@users.noreply.github.com>
2020-12-02 21:55:42 -0500

    Create Block.java
    The Block object. Contains functions to calculate and mine the hashes.

---
+++ src/main/java/com/model/Block.java
@@ -0,0 +1,40 @@
+public class Block { 
+ 
+    public String hash; 
+    public String parentHash; 
+    private String data; 
+    private int Nonce;
+    private boolean workProven;
+    public int leadingZeros = 0;
+    public String complexity = "0";
+  
+    // Constructor for the block 
+    public Block(String data, 
+                 String parentHash) 
+    { 
+        this.data = data; 
+        this.parentHash = parentHash; 
+        this.Nonce = 0; 
+        this.hash = calculateHash(); 
+    } 
+  
+    // Function to calculate the hash 
+    public String calculateHash(){
+      String calculatedHash = hashGenerator.generateHash(parentHash + Integer.toString(Nonce) + data);
+      return calculatedHash;
+     }
+    //Function to mine the hash for proof of work 
+    public String mineHash() 
+    {  
+      do{ 
+        String minedHash = calculateHash();
+        if(minedHash.substring(0,leadingZeros).equals(complexity)){
+          workProven = true;
+        }
+        else{
+          Nonce++;
+        }
+      }while (workProven = false);
+      return minedHash;
+    } 
+}
