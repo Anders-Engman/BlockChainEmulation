b6f664321298944fa7e25cf51ca57cca5362e586
rmathewes <75384595+rmathewes@users.noreply.github.com>
2020-12-02 19:45:12 -0500

    Update HashGenerator.java
    beginnings of sha256. we need to work on padding in the zeroes as proof of work.

--- src/main/java/com/model/HashGenerator.java
+++ src/main/java/com/model/HashGenerator.java
@@ -1,10 +1,38 @@
 package com.java.model;
-
+import java.security.MessageDigest; 
 public class HashGenerator {
     
-    public byte[] generateHashFromString() {
+    /*public byte[] generateHashFromString() {
         byte[] hash = new byte[64];
         // implementation
         return hash;
     }
+    */
+
+	public static String generateHash(String input) 
+	{ 
+		try { 
+			MessageDigest sha = MessageDigest.getInstance("SHA-256"); 
+			int i = 0; 
+
+			byte[] hash = sha.digest(input.getBytes("UTF-8")); 
+
+			// hexHash will contain 
+			// the Hexadecimal hash 
+			StringBuffer hexHash = new StringBuffer(); 
+
+			while (i < hash.length) { 
+				String hex = Integer.toHexString(0xff & hash[i]); 
+				if (hex.length() == 1) 
+					hexHash.append('0'); //that zero could be a variable imported in from somewhere.
+				hexHash.append(hex); 
+				i++; 
+			} 
+
+			return hexHash.toString(); 
+		} 
+		catch (Exception e) { 
+			throw new RuntimeException(e); 
+		} 
+	} 
 }
